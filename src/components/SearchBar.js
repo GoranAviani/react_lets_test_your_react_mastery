38462d86fad84dc2992f813594ce16538ec1b9b1
GoranAviani <goranaviani@gmail.com>
2022-04-15 12:06:47 +0200

    react_lets_test_your_react_mastery

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -18,3 +18,21 @@ class SearchBar extends React.Component{
         console.log(this.state.term)
     }
 
+    render() {
+        return(
+            <div className="search-bar ui segment">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
+                    <div className="field">
+                        <label>Video search</label>
+                        <input type="text" value={this.state.term} onChange={this.onInputChange}/>
+                    </div>
+
+
+
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
