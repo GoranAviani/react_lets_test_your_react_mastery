857bc06ae7e312a366e6e05730b1d0209d2a7525
GoranAviani <goranaviani@gmail.com>
2022-04-15 12:04:53 +0200

    react_lets_test_your_react_mastery

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
 
+const KEY = 'AIzaSyA2bxs44nmD9x_XTKl2q-zTfser9LgaPFg'
+
+
+
 export default axios.create(
     {
         baseURL: ' https://www.googleapis.com/youtube/v3',
