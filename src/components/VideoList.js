199c60e8bda24c22072dcab25793f6977315ca61
GoranAviani <goranaviani@gmail.com>
2022-04-15 12:06:58 +0200

    react_lets_test_your_react_mastery

---
+++ src/components/VideoList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import VideoItem from "./VideoItem";
+
+
+class VideoList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+    const renderedList = this.props.videos.map((video) => {
+        return <VideoItem key={video.id.videoId} video={video} onVideoSelect={this.props.onVideoSelect}/>;
+    });
+        return <div className="ui relaxed divided list">VIDEO LIST: {renderedList}</div>;
+
+    }
+}
+
+    export default VideoList;
